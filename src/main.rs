7eb56245987c3d0b94a9f5e2bccb994a022cbee4
Alexander Camuto <45801863+alexander-camuto@users.noreply.github.com>
2023-05-17 01:02:04 +0100

    issue repro

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use tract_onnx::{
+    prelude::{Framework, InferenceFact, InferenceModelExt, SymbolValues},
+    tract_core::ops::einsum::EinSum,
+};
+
+fn main() {
+    env_logger::init();
+    let reader = &mut std::fs::File::open("./src/network.onnx").unwrap();
+    let mut model = tract_onnx::onnx().model_for_read(reader).unwrap();
+
+    for (i, _) in model.clone().outputs.iter().enumerate() {
+        model.set_output_fact(i, InferenceFact::default()).unwrap();
+    }
+    // Note: do not optimize the model, as the layout will depend on underlying hardware
+    let model = model.into_typed().unwrap().into_decluttered().unwrap();
+    let batch_size = model.symbol_table.sym("batch_size");
+    let seq_len = model.symbol_table.sym("sequence_length");
+    model
+        .concretize_dims(&SymbolValues::default().with(&batch_size, 1))
+        .unwrap()
+        .concretize_dims(&SymbolValues::default().with(&seq_len, 1))
+        .unwrap();
+
+    for node in model.nodes() {
+        // Extract the slope layer hyperparams
+        match node.op().downcast_ref::<EinSum>() {
+            // this will not be valid
+            Some(einsum) => println!("{}", einsum.axes),
+            None => {}
+        };
+    }
+}
